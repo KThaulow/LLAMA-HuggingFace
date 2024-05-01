8c33b92764707d4b85220a2f3eef39a54965010f
Kristian Thaulow <kristian1990@gmail.com>
2024-05-01 17:53:46 +0000

    Requirements

--- llama2.py
+++ llama2.py
@@ -6,6 +6,7 @@ from transformers import (AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfi
 
 # Loading the model
 llama_model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path = "aboonaji/llama2finetune-v2",
+                                                   device_map='mps',
                                                    quantization_config = BitsAndBytesConfig(load_in_4bit = True, bnb_4bit_compute_dtype = getattr(torch, "float16"), bnb_4bit_quant_type = "nf4"))
 llama_model.config.use_cache = False
 llama_model.config.pretraining_tp = 1
